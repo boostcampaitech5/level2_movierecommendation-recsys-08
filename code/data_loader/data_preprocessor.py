b12be092831f55e1e390151b410d02a0addfcd56
Park SuHyun <golfpak0311@naver.com>
2023-06-19 01:22:43 +0000

    [Feat] Complete data_loader directory

--- code/data_loader/data_preprocessor.py
+++ code/data_loader/data_preprocessor.py
@@ -1,4 +1,7 @@
 from abc import abstractmethod
+from scipy.sparse import csr_matrix
+import numpy as np
+import pandas as pd
 
 
 class BaseDataPreprocessor:
@@ -18,3 +21,55 @@ class TempDataPreprocessor(BaseDataPreprocessor):
 
     def preprocessing(self, args):
         pass
+
+
+class TransformerDataPreprocessor(BaseDataPreprocessor):
+    def __init__(self, args):
+        super().__init__()
+        args.data_file = args.data_dir + "train_ratings.csv"
+        self.preprocessing(args)
+
+    def preprocessing(self, args):
+        self.get_user_sequences(args)
+        args.valid_rating_matrix = self.generate_rating_matrix(args, "valid")
+        args.test_rating_matrix = self.generate_rating_matrix(args, "test")
+        args.submission_rating_matrix = self.generate_rating_matrix(args, "submission")
+
+    def get_user_sequences(self, args):
+        df = pd.read_csv(args.data_file)
+        df = df.sort_values(by=["user", "time"], axis=0)
+
+        args.user_sequences = df.groupby("user")["item"].apply(list).to_list()
+        args.users = df["user"].unique()
+        args.num_users = len(args.user_sequences)
+        args.num_items = df["item"].max() + 2  # padding, masking(pretrain)
+
+    def generate_rating_matrix(self, args, mode="valid"):
+        row = []
+        col = []
+        data = []
+
+        for user_id, item_list in enumerate(args.user_sequences):
+            if mode == "valid":
+                item_list = item_list[:-2]
+
+            elif mode == "test":
+                item_list = item_list[:-1]
+
+            else:
+                item_list = item_list[:]
+
+            for item in item_list:
+                row.append(user_id)
+                col.append(item)
+                data.append(1)
+
+        row = np.array(row)
+        col = np.array(col)
+        data = np.array(data)
+
+        rating_matrix = csr_matrix(
+            (data, (row, col)), shape=(args.num_users, args.num_items)
+        )
+
+        return rating_matrix
