568e7a99de8c6a1e5512876751e98a7dbe7b57ff
Park SuHyun <golfpak0311@naver.com>
2023-06-19 01:26:09 +0000

    [Feat] Complete trainer directory

--- code/trainer/optimizer.py
+++ code/trainer/optimizer.py
@@ -13,4 +13,12 @@ def get_optimizer(args):
             args.model.parameters(), lr=args.lr, weight_decay=args.weight_decay
         )
 
+    elif args.optimizer_title == "Adam_betas":
+        optimizer = Adam(
+            args.model.parameters(),
+            lr=args.lr,
+            betas=(args.adam_beta1, args.adam_beta2),
+            weight_decay=args.weight_decay,
+        )
+
     return optimizer
